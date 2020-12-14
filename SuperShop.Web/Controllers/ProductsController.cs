36103989dbb14bc27e2939f95b1c34644e24fcdb
Akos Nagy <nagy.akos.bme.2009@gmail.com>
2020-12-14 11:28:42 +0100

    Dummy view

---
+++ SuperShop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET; body={}; url: /Products/Index
+        public IActionResult Index()
+        {
+            Product model = new Product
+            {
+                ProductName = "Labda",
+                UnitPrice = 10,
+                UnitsInStock = 1
+            };
+
+            return View(model);
+        }
+    }
+}
